37e19fa30fe859332709a65d9923776c4cef99e4
Nazmus Sakib <150610050+Sakib2405@users.noreply.github.com>
2024-12-13 18:43:37 +0600

    Create Problem-1001.cpp

---
+++ Beecrowd/Problem-1001.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+int a,b,x;
+cout<<"a=    " ;
+cin>>a;
+cout<<endl;
+cout<<"b=    ";
+cin>>b;
+x=a+b;
+
+cout<<endl<<"   x=   "<<x<<endl;
+return 0;
+}
