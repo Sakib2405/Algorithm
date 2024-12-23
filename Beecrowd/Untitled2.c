0ed0906184cf827899dd0277451e011208f6cea8
Nazmus Sakib <150610050+Sakib2405@users.noreply.github.com>
2024-12-23 21:34:34 +0600

    Create Untitled2.c

---
+++ Beecrowd/Untitled2.c
@@ -0,0 +1,11 @@
+// Suppose there are giver two integer number a & b. Now you division this two integer number.
+
+#include <stdio.h>
+int main(){
+int a,b; float div;
+printf("enter the value a&b:");
+scanf("%d%d",&a,&b);
+ div=(float)a/b;
+printf("The div is :%f",div);
+return 0;
+}
